3924f2f0f1707dadf91cca82fb6c3bc3d1ece4b4
Angel Hurtado <sudo@angelxehg.com>
2021-06-26 16:49:48 -0500

    Datetime element examples (from https://github.com/ionic-team/ionic-framework/blob/v6.0.0-beta.0/core/src/components/datetime/usage/angular.md)

--- src/app/pages/datetime/datetime.page.ts
+++ src/app/pages/datetime/datetime.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-datetime',
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatetimePage implements OnInit {
 
+  @ViewChild('customDatetime', { static: false }) datetime: HTMLIonDatetimeElement;
   constructor() { }
+
+  confirm() {
+    this.datetime.confirm();
+  }
+
+  reset() {
+    this.datetime.reset();
+  }
 
   ngOnInit() {
   }
