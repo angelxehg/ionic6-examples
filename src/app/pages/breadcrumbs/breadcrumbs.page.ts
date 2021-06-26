6813041b8f0a236d1524fe1bc0ff0568259bde77
Angel Hurtado <sudo@angelxehg.com>
2021-06-26 16:42:46 -0500

    Breadcrumbs examples (from https://github.com/ionic-team/ionic-framework/blob/v6.0.0-beta.0/core/src/components/breadcrumbs/usage/angular.md)

--- src/app/pages/breadcrumbs/breadcrumbs.page.ts
+++ src/app/pages/breadcrumbs/breadcrumbs.page.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsPage implements OnInit {
 
+  maxBreadcrumbs = 4;
+
   constructor() { }
+
+  expandBreadcrumbs() {
+    this.maxBreadcrumbs = undefined;
+  }
 
   ngOnInit() {
   }
